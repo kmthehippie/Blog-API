f9b52bca5b34418106c448c03b74cbdfe938fb39
kmthehippie <66264446+kmthehippie@users.noreply.github.com>
2024-06-05 17:42:03 +0800

    Update Dockerfile

--- app.js
+++ app.js
@@ -104,7 +104,7 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'dist', 'index.html'));
   });
 
-app.listen(port, ()=>{
+app.listen(port, "0.0.0.0", ()=>{
     "App is listening on port ",port
 })
 //Export App
