ff1cc9dac84dffa6d0813b61c48ae2f37bf14502
kmthehippie <66264446+kmthehippie@users.noreply.github.com>
2024-06-05 17:29:11 +0800

    Update for deployment

--- config/allowed_origins.js
+++ config/allowed_origins.js
@@ -3,7 +3,7 @@ const allowedOrigins = [
     "http://localhost:5173",
     "http://127.0.0.1:5500",
     "https://km-blog-app-2024.fly.dev/",
-    "https://0:0:0:0:3000"
+    "https://km-blogs.netlify.app"
     // future glitch or flyio link
 ]
 
